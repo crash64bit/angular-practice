dd9e815e73083c2d60c5e50e81329bf3acf62d08
crash64bit <crash64bit@gmail.com>
2025-10-10 14:54:22 +0300

    Remove todo servive, merge todo items and item

--- src/app/pages/todo-page/todo-page.component.ts
+++ src/app/pages/todo-page/todo-page.component.ts
@@ -1,8 +1,4 @@
-import { Component, inject } from '@angular/core';
-import { TodoService } from '../../services/todo.servive';
-import { ButtonModule } from 'primeng/button';
-import { InputTextModule } from 'primeng/inputtext';
-import { InputGroupModule } from 'primeng/inputgroup';
+import { Component } from '@angular/core';
 import { TodoItemsComponent } from '../../todo-items/todo-items.component';
 
 
@@ -10,13 +6,9 @@ import { TodoItemsComponent } from '../../todo-items/todo-items.component';
   selector: 'todo-page',
   imports: [
     TodoItemsComponent,
-    ButtonModule,
-    InputTextModule,
-    InputGroupModule,
   ],
   templateUrl: './todo-page.component.html',
   styleUrl: './todo-page.component.scss'
 })
 export class TodoPageComponent {
-  todoService = inject(TodoService);
 }
--- src/app/todo-items/todo-items.component.ts
+++ src/app/todo-items/todo-items.component.ts
@@ -1,19 +1,95 @@
 import { Component, inject } from '@angular/core';
-import { TodoService } from '../services/todo.servive';
-import { TodoItemComponent } from "../todo-item/todo-item.component";
+import { BrowserStorageService } from '../services/storage.servive';
+import { CheckboxChangeEvent, Checkbox } from 'primeng/checkbox';
+import { Todo } from '../interfaces/todo';
+import { Button } from "primeng/button";
+import { CheckboxModule } from 'primeng/checkbox';
+import { FormsModule } from '@angular/forms';
+import { ButtonModule } from 'primeng/button';
+
+import { InputTextModule } from 'primeng/inputtext';
+import { InputGroupModule } from 'primeng/inputgroup';
+
+
 
 @Component({
   selector: 'todo-items',
-  imports: [TodoItemComponent, TodoItemComponent],
+  imports: [Checkbox, Button, CheckboxModule, FormsModule, ButtonModule, InputTextModule, InputGroupModule],
   templateUrl: './todo-items.component.html',
   styleUrl: './todo-items.component.scss'
 })
 export class TodoItemsComponent {
-  todoService = inject(TodoService);
 
-  // getTodoItems(): Array<object> {
-  //   return this.todoService.getTodoItems();
-  // }
+  storageService = inject(BrowserStorageService);
+  
+  inputValue: string = '';
+  
+  onInputChange(event: Event): void {
+    const value = (event.target as HTMLInputElement).value;
+    this.inputValue = value;
+  }
 
-  // items: Array<object> = this.getTodoItems();
+  getTodoItems() {
+    const rawItems = this.storageService.get('todo');
+    if (!rawItems || !Array.isArray( JSON.parse(rawItems) )) {
+      return [];
+    }
+
+    return JSON.parse(rawItems);
+  }
+
+  updateTodoItemChecked(index: number, event: CheckboxChangeEvent): void {
+
+    const checked = event.checked;
+
+    const items = this.getTodoItems();
+
+    const item = items[index];
+
+    const itemNew: Todo = {
+      checked: checked,
+      value: item.value
+    };
+
+    items.splice(index, 1, itemNew);
+
+    this.updateTodoItems(items);
+  }
+
+  removeTodoItem(index: number): void {
+
+    const items = this.getTodoItems();
+    
+    items.splice(index, 1);
+
+    this.updateTodoItems(items);
+    
+  }
+
+  addTodoItem(): void {
+    if (this.inputValue == '') return;
+
+    const items = this.getTodoItems();
+
+    const item: Todo = {
+      checked: false,
+      value: this.inputValue
+    };
+
+    items.push(item);
+
+    this.updateTodoItems(items);
+
+    this.inputValue = '';
+  }
+
+  updateTodoItems(items: object): void {
+    this.storageService.set('todo', JSON.stringify(items));
+  }
+
+  items: Todo[] = this.getTodoItems();
+  
+  ngOnInit() {
+    // items: Todo[] = this.getTodoItems(); ?
+  }
 }
